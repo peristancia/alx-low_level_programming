34771f2d6cacdc26fd84c4b184e68721f4f85c1d
peristancia <peristancia@gmail.com>
2023-07-13 14:35:09 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,10 @@
-#!/bin/bash
-
+#include<stdio.h>
+/**
+ * main - Aprogram that print a line using the printf function
+ * Return: 0 (success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n")
+return (0);
+}
