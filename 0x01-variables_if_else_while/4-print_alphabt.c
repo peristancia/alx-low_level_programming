fed0b55aed79a58e405c10c6df40abae13e39db9
peristancia <peristancia@gmail.com>
2023-07-15 06:54:45 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * * Description "print alphabets except q and e"
+ * * Description: 'print alphabets except q and e'
  * Return: Always 0(success)
  */
 int main(void)
