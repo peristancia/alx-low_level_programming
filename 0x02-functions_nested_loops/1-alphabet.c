9b18e1ca117dc2b27e9a8d6c59e3a97da3a80a8f
peristancia <peristancia@gmail.com>
2023-07-18 07:33:38 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Description: print the alphabets in lowercase
+ */
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; leter++)
+		_putchar(letter);
+	_putchar('\n');
+}
