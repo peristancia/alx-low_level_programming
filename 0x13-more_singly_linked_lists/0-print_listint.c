7ba205ea033b8b8d2e769af8cef2a42b67cd93b1
peristancia <peristancia@gmail.com>
2023-08-29 03:52:04 -0700

    0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements of linked lists
+ * @h: type of listint to print
+ *
+ * Return: number of nodes to be printed
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t num = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		num++;
+		h = h->next;
+	}
+	
+	return (num);
+}
