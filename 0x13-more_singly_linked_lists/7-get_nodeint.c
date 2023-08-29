1e0df666ef4c24dc8ed40d800769d1212dd7dc7e
peristancia <peristancia@gmail.com>
2023-08-29 09:26:26 -0700

    10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -5,8 +5,9 @@
  * @head: first node
  * @index: node to be returned
  *
- * Return: pointer to the node starting at 0, or NULL if it does not exist
- */
+ * Return: pointer to the node starting at 0, or NULL if it does not exis#include "lists.h"
+
+*/
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i = 0;
